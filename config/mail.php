93dda6e5813ef4dc7c3d4943ac4ee591ec6bdf2e
ferry <ferryaze1@gmail.com>
2018-07-30 11:24:56 +0900

    update email config

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'info.weworktogether.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'info.weworktogether@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'We Work Together'),
     ],
 
